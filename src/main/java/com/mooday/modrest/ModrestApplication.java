cc1511a7fdc9b544ba0f738a30b171fd908bf778
Pablo Naveira López <pablonaveiralopez@gmail.com>
2024-08-04 14:35:56 +0200

    first commit

---
+++ src/main/java/com/mooday/modrest/ModrestApplication.java
@@ -0,0 +1,13 @@
+package com.mooday.modrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mooday/modrest/ModrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooday.modrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
